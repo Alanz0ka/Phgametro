1fc9bfd3930cc891c3122016b48eb4d3d83d9335
enxerga <104922063+Alanz0ka@users.noreply.github.com>
2023-08-24 12:03:42 -0300

    script de configuração

--- config-phgametro/config-phgametro.ino
+++ config-phgametro/config-phgametro.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
 
+ // Código para calibração
+// Converte valor analógico em tensão
+ 
+int entrada_A0;
+float tensao, soma = 0.0;
+int i = 0;
+void setup()  {
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  entrada_A0 = analogRead(A0);
+  tensao = (entrada_A0 * 5.0) / 1024.0;
+  soma += tensao;
+  if (i == 50){
+    Serial.println(soma /50);
+    soma = 0;
+    i = 0;
+  }
+  i++;
+    
+  delay(10);
 }
